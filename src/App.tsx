71f1884ad758cc47badb4323d2bc0e80d99577cb
Taisei <taisei96.web@gmail.com>
2023-11-04 18:13:23 +0900

    Todoリスト完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, FormEvent, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputValue, setInputValue] = useState("")
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  type Todo = {
+    inputValue: string;
+    id: number;
+    checked: boolean;
+  }
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    // console.log(e.target.value)
+    setInputValue(e.target.value);
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // 新しいTodoを作成
+    const newTodo: Todo = {
+      inputValue: inputValue,
+      id: todos.length,
+      checked: false,
+    }
+
+    setTodos([...todos, newTodo])
+    setInputValue("");
+  }
+
+  const handleEdit = (id: number, inputValue: string) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.inputValue = inputValue;
+      }
+      return todo;
+    });
+
+    setTodos(newTodos);
+  }
+
+  const handleChecked = (id: number, checked: boolean) => {
+     const newTodos = todos.map((todo) => {
+       if (todo.id === id) {
+         todo.checked = !checked;
+       }
+       return todo;
+     });
+
+     setTodos(newTodos);
+  }
+
+  const handleDelete = (id: number) => {
+    const newTodos = todos.filter((todo) => todo.id !== id)
+    setTodos(newTodos);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="App">
+        <div>
+          <h2>Todoリスト with TypeScript</h2>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <input
+              type="text"
+              onChange={(e) => handleChange(e)}
+              className="inputText"
+            />
+            <input type="submit" value="作成" className="submitButton" />
+          </form>
+          <ul className="todoList">
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                {/* {todo.inputValue} */}
+                <input
+                  type="text"
+                  onChange={(e) => handleEdit(todo.id, e.target.value)}
+                  className="inputText"
+                  value={todo.inputValue}
+                  disabled={todo.checked}
+                />
+                <input
+                  type="checkbox"
+                  onChange={() => handleChecked(todo.id, todo.checked)}
+                />
+                <button onClick={() => handleDelete(todo.id) }>消す</button>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
